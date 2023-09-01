a6b38baaea7387e78722f3be242beae2b0a5a23a
Nayan Parmar <nparmar84@gmail.com>
2023-09-01 13:53:17 -0700

    Added contact form challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AccordionApp from "./accordion/AccordionApp.jsx";
+import ContactFormApp from "./contact-form/ContactFormApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AccordionApp />
+    {/* <AccordionApp /> */}
+    <ContactFormApp />
   </React.StrictMode>
 );
