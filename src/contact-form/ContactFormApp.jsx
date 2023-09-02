f4000480cc6ee98841d6304ccedfa93a2080cfce
Nayan Parmar <nparmar84@gmail.com>
2023-09-01 17:02:21 -0700

    Updated contact form

--- src/contact-form/ContactFormApp.jsx
+++ src/contact-form/ContactFormApp.jsx
@@ -1,5 +1,28 @@
-import ContactForm from "./ContactForm";
+import "./styles.css";
+import submitForm from "./submitForm";
 
 export default function ContactFormApp() {
-  return <ContactForm />;
+  return (
+    <form
+      onSubmit={submitForm}
+      action="https://www.greatfrontend.com/api/questions/contact-form"
+      method="post"
+    >
+      <div>
+        <label htmlFor="name-input">Name</label>
+        <input id="name-input" name="name" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email-input">Email</label>
+        <input id="email-input" name="email" type="email" />
+      </div>
+      <div>
+        <label htmlFor="message-input">Message</label>
+        <textarea id="message-input" name="message" />
+      </div>
+      <div>
+        <button>Send</button>
+      </div>
+    </form>
+  );
 }
