c244c6ae5cdef588292609a50614eda821eef246
Nayan Parmar <nparmar84@gmail.com>
2023-09-03 18:38:16 -0700

    Added flight booker

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AccordionApp from "./accordion/AccordionApp.jsx";
 import ContactFormApp from "./contact-form/ContactFormApp.jsx";
+import FlightBookerApp from "./flight-booker/FlightBookerApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <AccordionApp /> */}
-    <ContactFormApp />
+    {/* <ContactFormApp /> */}
+    <FlightBookerApp />
   </React.StrictMode>
 );
