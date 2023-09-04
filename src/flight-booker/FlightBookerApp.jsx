a2306102e17839768744f3650acdd08cc26156c3
Nayan Parmar <nparmar84@gmail.com>
2023-09-03 20:28:06 -0700

    Added form, selections and booking button

--- src/flight-booker/FlightBookerApp.jsx
+++ src/flight-booker/FlightBookerApp.jsx
@@ -1,5 +1,43 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function FlightBookerApp() {
-  return <div>Helloooooooooo</div>;
+  const [flightOption, setFlightOption] = useState("one-way");
+
+  return (
+    <div>
+      <form
+        className="flight-booker"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <select
+          value={flightOption}
+          onChange={(e) => {
+            setFlightOption(e.target.value);
+            console.log(e.target.value);
+          }}
+        >
+          <option value="one-way">One-way flight</option>
+          <option value="return-flight">Return flight</option>
+        </select>
+        {/* <input
+          aria-label="Departure date"
+          type="date"
+          value={""}
+          onChange={(e) => {}}
+        />
+        {false && (
+          <input
+            aria-label="Return date"
+            type="date"
+            value={""}
+            onChange={(e) => {}}
+          />
+        )} */}
+        <button>Book</button>
+      </form>
+    </div>
+  );
 }
