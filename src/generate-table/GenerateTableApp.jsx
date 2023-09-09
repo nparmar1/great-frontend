e00f9945f37cd68cc5728bb9215e9ae3b3e76d4e
Nayan Parmar <nparmar84@gmail.com>
2023-09-08 21:04:29 -0700

    Added form functionalty

--- src/generate-table/GenerateTableApp.jsx
+++ src/generate-table/GenerateTableApp.jsx
@@ -1,7 +1,49 @@
-export default function GenerateTableApp(){
-    return (
+import { useState } from "react";
+import Table from "./Table";
+
+import "./styles.css";
+
+export default function GenerateTableApp() {
+  const [rows, setRows] = useState("");
+  const [columns, setColumns] = useState("");
+
+  return (
+    <div className="app">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const data = new FormData(e.target);
+          const rows = data.get("rows");
+          setRows(Number(rows));
+          const columns = data.get("columns");
+          setColumns(columns);
+          
+        }}
+      >
         <div>
-            Hello, World!
+          <label htmlFor="rows">Rows</label>
+          <input
+            id="rows"
+            name="rows"
+            type="number"
+            min={1}
+            defaultValue={rows}
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="columns">Columns</label>
+          <input
+            id="columns"
+            name="columns"
+            type="number"
+            min={1}
+            defaultValue={columns}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {Boolean(rows) && Boolean(columns) && <Table />}
+    </div>
+  );
 }
