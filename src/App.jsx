cd252e988af62bae9b8c1d963b7fc30a0083b345
Nayan Parmar <nparmar84@gmail.com>
2023-09-21 14:03:19 -0700

    Completed fetch hacker news

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import AccordionApp from "./accordion/AccordionApp.jsx";
 import ContactFormApp from "./contact-form/ContactFormApp.jsx";
 import FlightBookerApp from "./flight-booker/FlightBookerApp.jsx";
-import HackerNewsApp from "./fetch-hacker-news/HackerNewsApp.jsx";
 import GenerateTableApp from "./generate-table/generateTableApp.jsx";
+import { HackerNewsApp } from "./fetch-hacker-news/HackerNewsApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
