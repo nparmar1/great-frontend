e532facb006eba62940d3c640f6dfc87eb325176
Nayan Parmar <nparmar84@gmail.com>
2023-09-21 18:16:04 -0700

    Still working on board

--- src/tic-tac-toe/TicTacToeApp.jsx
+++ src/tic-tac-toe/TicTacToeApp.jsx
@@ -1,21 +1,23 @@
+import { useState } from "react";
 import "./styles.css";
 
+function Cell() {
+  return <>Hii</>;
+}
+
 export default function TicTacToeApp() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
   return (
     <div className="app">
-        <div className="board">
-            {
-                Array(9)
-                .fill(null)
-                .map((_, index) => index)
-                .map((cellIndex) => {
-                    const turn = xIsPlaying ? 'X' : 'O';
-                    return (
-
-                    )
-                })
-            }
-        </div>
+      <div className="board">
+        {Array(9)
+          .fill(null)
+          .map((_, index) => index)
+          .map((cellIndex) => {
+            return <Cell key={cellIndex} />;
+          })}
+      </div>
     </div>
-  )
+  );
 }
