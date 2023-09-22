a20e7b122b2236c0cc8927a04310d49214535494
Nayan Parmar <nparmar84@gmail.com>
2023-09-22 15:29:02 -0700

    Completed tabs

--- src/tabs/Tabs.jsx
+++ src/tabs/Tabs.jsx
@@ -1,3 +1,40 @@
-export default function Tabs() {
-  return <div>Hello!!</div>;
+import { useState } from "react";
+
+export default function Tabs({ defaultValue, items }) {
+  const [value, setValue] = useState(defaultValue ?? items[0].value);
+
+  return (
+    <div className="tabs">
+      <div className="tabs-list">
+        {items.map(({ label, value: itemValue }) => {
+          const isActiveValue = itemValue === value;
+
+          return (
+            <button
+              key={itemValue}
+              type="button"
+              onClick={() => {
+                setValue(itemValue);
+              }}
+              className={[
+                "tabs-list-item",
+                isActiveValue && "tabs-list-item--active",
+              ]
+                .filter(Boolean)
+                .join(" ")}
+            >
+              {label}
+            </button>
+          );
+        })}
+      </div>
+      <div>
+        {items.map(({panel, value: itemValue}) => (
+          <div key={itemValue} hidden={itemValue !== value}>
+            {panel}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
