4f372067f4558f45ff76b1d7b67f59d66dad7de1
Nayan Parmar <nparmar84@gmail.com>
2023-10-02 14:15:35 -0700

    Continuing tic-tac-toe

--- src/tic-tac-toe/TicTacToeApp.jsx
+++ src/tic-tac-toe/TicTacToeApp.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./styles.css";
 
-function Cell() {
-  return <>Hii</>;
+function Cell(){
+  return <>Hii</>
 }
 
-export default function TicTacToeApp() {
+export default function TicTacToeApp(){
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsPlaying, setIsXplaying] = useState(true);
 
   return (
     <div className="app">
@@ -15,9 +16,20 @@ export default function TicTacToeApp() {
           .fill(null)
           .map((_, index) => index)
           .map((cellIndex) => {
-            return <Cell key={cellIndex} />;
-          })}
+            const turn = xIsPlaying ? 'X' : 'O'
+            return (
+              <Cell 
+                key={cellIndex}
+                disabled={board[cellIndex] != null || ''}
+                index={cellIndex}
+                mark={board[cellIndex]}
+                turn={turn}
+                
+              />
+            )
+          })
+        }
       </div>
     </div>
-  );
+  )
 }
