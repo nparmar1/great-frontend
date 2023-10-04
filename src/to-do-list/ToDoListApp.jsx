03e4552aa58081ef5cf89be0f8c151a24fbef05d
Nayan Parmar <nparmar84@gmail.com>
2023-10-03 18:07:11 -0700

    Added input field and submit button

--- src/to-do-list/ToDoListApp.jsx
+++ src/to-do-list/ToDoListApp.jsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
+
+let id = 0;
+
+const INITIAL_TASKS = [
+  { id: id++, label: "Walk the dog" },
+  { id: id++, label: "Water the plants" },
+  { id: id++, label: "Wash the dishes" },
+];
+
 export default function ToDoListApp() {
+  const [tasks, setTasks] = useState(INITIAL_TASKS);
+  const [newTask, setNewTask] = useState("");
+
   return (
     <div>
-        <h1>Todo List</h1>
+      <h1>To Do List</h1>
+      <div>
+        <input
+          aria-label="Add new Task"
+          type="text"
+          placeholder="Add your task"
+          value={newTask}
+          onChange={(event) => {
+            setNewTask(event.target.value);
+          }}
+        />
         <div>
-            <input
-                aria-label="Add new task"
-                type="text"
-                placeholder="Add your task"
-            />
+            <button>
+                Submit
+            </button>
         </div>
+      </div>
+      <ul>
+        {tasks.map(({ id, label }) => (
+            <li></li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
