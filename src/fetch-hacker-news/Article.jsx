f9a5b54c68b02bdc9848b1a0106007a470fbd173
Nayan Parmar <nparmar84@gmail.com>
2023-10-16 17:52:21 -0700

    Completed hacker news

--- src/fetch-hacker-news/Article.jsx
+++ src/fetch-hacker-news/Article.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
-export default function Article({ id, onClick }) {
-  const [article, setArticle] = useState({});
+export default function Article({ id, deleteIds }) {
+  const [article, setArticle] = useState("");
 
   useEffect(() => {
     async function fetchId() {
@@ -9,8 +9,8 @@ export default function Article({ id, onClick }) {
         `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
       );
       const data = await response.json();
-      const { by: author, title, url } = data;
 
+      const { by: author, title, url } = data;
       setArticle({ author, title, url });
     }
 
@@ -18,16 +18,19 @@ export default function Article({ id, onClick }) {
   }, []);
 
   return (
-    <div style={{
-      border: '1px solid #555',
-      margin: "8px",
-      borderRadius: '5px',
-      padding: '8px'
-    }}>
-        <h1>{article.title}</h1>
-        <a href={article.url}>{article.url}</a>
-        <p>{article.author}</p>
-        <button onClick={() => onClick(id)}>Delete</button>
+    <div
+      style={{
+        border: "1px solid #555",
+        margin: "8px",
+        borderRadius: "5px",
+        backgroundColor: "#fff",
+        padding: "8px",
+      }}
+    >
+      <h1>{article.title}</h1>
+      <a href={article.url}>{article.url}</a>
+      <p>by {article.author}</p>
+      <button onClick={() => deleteIds(id)}>Delete</button>
     </div>
-  )
+  );
 }
