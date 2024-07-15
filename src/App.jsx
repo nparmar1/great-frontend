dafe1726877cb43c5a3902542f04127996f1a296
Nayan Parmar <nparmar84@gmail.com>
2024-07-15 14:21:15 -0700

    Started search auto complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import TicTacToeApp from './tic-tac-toe/TicTacToeApp.jsx';
+// import TicTacToeApp from './tic-tac-toe/TicTacToeApp.jsx';
 // import TabsApp from "./tabs/TabsApp.jsx";
 // import AccordionApp from "./accordion/AccordionApp.jsx";
 // import GridLightsApp from "./grid-lights/GridLightsApp.jsx";
@@ -13,7 +13,6 @@ import TicTacToeApp from './tic-tac-toe/TicTacToeApp.jsx';
 // import FlightBookerApp from "./flight-booker/FlightBookerApp.jsx";
 // import HackerNewsApp from "./fetch-hacker-news/HackerNewsApp.jsx";
 // import GenerateTableApp from "./generate-table/generateTableApp.jsx";
-// import SearchAppTwo from "./search-auto-complete-2/SearchAppTwo.jsx";
 // import PhotoCarouselApp from "./photo-carousel/PhotoCarouselApp.jsx";
 // import StarRatingApp from "./star-rating/StarRatingApp.jsx";
 // import FetchBreedListApp from "./fetch-breed-list/FetchBreedListApp.jsx";
@@ -27,10 +26,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <HackerNewsApp /> */}
     {/* <TabsApp /> */}
     {/* <SearchApp /> */}
-    <TicTacToeApp />
+    {/* <TicTacToeApp /> */}
     {/* <FetchBreedListApp /> */}
     {/* <GridLightsApp /> */}
-    {/* <SearchAppTwo /> */}
     {/* <PhotoCarouselApp /> */}
     {/* <DataTableApp /> */}
     {/* <StarRatingApp /> */}
