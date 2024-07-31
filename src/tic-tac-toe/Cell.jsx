6572d11918ac14a0470abb93bfc908a6ab5988da
Nayan Parmar <nparmar84@gmail.com>
2024-07-30 17:46:11 -0700

    Started tic-tac-toe

--- src/tic-tac-toe/Cell.jsx
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-
-export default function Cell({ mark, onClick, disabled }) {
-  return (
-    <button className="cell" disabled={disabled} onClick={onClick}>
-      {mark}
-    </button>
-  );
-}
